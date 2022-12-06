456a77a85a8a14c7f6e2cc4e01adce55929ede6b
Nathalia Schmucler <108892573+nathaliasch@users.noreply.github.com>
2022-12-05 23:20:30 -0300

    Delete 3_answer_nathalia_schmucler.sql

--- 3_answer_nathalia_schmucler.sql
+++
@@ -1,4 +0,0 @@
-select extract (month from rental_date) as mes_entrada_cliente, count (distinct customer_id) as quantidade_de_cliente 
-from rental
-group by mes_entrada_cliente
-order by mes_entrada_cliente desc limit 3;
